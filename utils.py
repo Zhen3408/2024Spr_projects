798222e5479a10b97cb05244cc0ae23194abfacb
Zhen3408 <lizhen3408@gmail.com>
2024-04-30 23:10:16 -0500

    function revise

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import pandas as pd
 import os
 import calendar
 
-
 # Define global variable:
 isced_education_mapping = {
     'Less than primary education': 'Less than a high school diploma',
@@ -20,7 +19,6 @@ isced_education_mapping = {
     'Lower secondary education': 'Less than a high school diploma',
     'Completion of intermediate lower secondary programmes': 'Less than a high school diploma'
 }
-
 
 european_countries = [
     'Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belarus', 'Belgium',
@@ -43,7 +41,24 @@ education_level_order = [
 ]
 
 
-def read_usbls_data(dir_path: str, file_name: str, blank_row: int):
+def read_usbls_data(dir_path: str, file_name: str, blank_row: int) -> pd.DataFrame:
+    """
+       Reads an Excel file containing US Bureau of Labor Statistics data from the specified directory,
+       skips the rows up to the specified blank row, and restructures the data into a long format with
+       months and corresponding values under each age group (inferred from the file name) as columns.
+
+       Parameters:
+       dir_path (str): The directory path where the Excel file is located.
+       file_name (str): The name of the Excel file, including the extension. The part before the extension
+                        is used to label the data column for the age group.
+       blank_row (int): The number of initial rows to skip in the Excel file (0-indexed), typically used to
+                        skip header information or blank rows.
+
+       Returns:
+       pandas.DataFrame: A DataFrame indexed by 'Year' and 'Month' columns, with one additional column
+                         representing the data for the specific age group, labeled by the file name prefix.
+
+       """
     # Construct the full file path
     full_path = os.path.join(dir_path, file_name)
     # Read the Excel file
@@ -74,7 +89,6 @@ def concatenate_usbls_files(dir_path: str, blank_row: int, year_range: tuple = (
         else:
             combined_df = pd.merge(combined_df, df, left_index=True, right_index=True, how='right')
     combined_df.sort_index(inplace=True)
-
     # Filter the data based on the year range
     combined_df = combined_df.loc[start_year:end_year]
     return combined_df
@@ -95,7 +109,7 @@ def merge_eurostat_data(dir_path: str) -> pd.DataFrame:
     combined_df = None
     for file_name in os.listdir(dir_path):
         if file_name.endswith('.xlsx'):
-                new_df = read_eurostat_data(dir_path, file_name)
+            new_df = read_eurostat_data(dir_path, file_name)
         if combined_df is None:
             combined_df = new_df
         else:
@@ -103,8 +117,31 @@ def merge_eurostat_data(dir_path: str) -> pd.DataFrame:
     return combined_df
 
 
-def process_onsgovuk_data(file_path: str) -> pd.DataFrame:
-    df_uk = pd.read_csv(file_path)
+def process_onsgovuk_data(df_uk: pd.DataFrame) -> pd.DataFrame:
+    """
+    Processes data from the ONS (Office for National Statistics, UK) to calculate the average unemployment rates
+    by ethnicity over time. This function cleans the data, converts time strings to datetime objects, ensures numeric
+    representation of unemployment values, groups data by time and ethnicity, and then restructures it into a pivot table format.
+
+    Parameters:
+    df_uk (pd.DataFrame): A DataFrame containing UK unemployment data from OnsGovUK.
+
+    Returns:
+    pd.DataFrame: A pivot table DataFrame indexed by 'time' (datetime), with columns for each ethnicity category, containing
+                  average unemployment rates.
+
+    DocTest:
+    >>> data = {'time': ['Jan2020', 'Jan2020', 'Feb2020', 'Feb2020'],
+    ...         'ethnicity': ['Asian', 'Black', 'Asian', 'Black'],
+    ...         'value': ['5', '6', '4', '7']}
+    >>> df_uk = pd.DataFrame(data)
+    >>> processed_df = process_onsgovuk_data(df_uk)
+    >>> print(repr(processed_df))
+    ethnicity   Asian  Black
+    time
+    2020-01-01    5.0    6.0
+    2020-02-01    4.0    7.0
+    """
     df_uk['time'] = pd.to_datetime(df_uk['time'].str.split('-').str[0], format='%b%Y')  # Extract the month and year
     # Ensure the 'value' column is numeric
     df_uk['value'] = pd.to_numeric(df_uk['value'], errors='coerce')
@@ -112,14 +149,55 @@ def process_onsgovuk_data(file_path: str) -> pd.DataFrame:
     # regions
     average_unemployment_by_race = df_uk.groupby(['time', 'ethnicity'])['value'].mean().reset_index()
     # Pivot the table again to get the desired format with 'time' as the index
-    average_unemployment_by_race = average_unemployment_by_race.pivot_table(index='time', columns='ethnicity', values='value')
-    average_unemployment_by_race = average_unemployment_by_race.filter(items=['Asian', 'Black', 'White', 'Mixed', 'Other'])
+    average_unemployment_by_race = average_unemployment_by_race.pivot_table(index='time', columns='ethnicity',
+                                                                            values='value')
+    average_unemployment_by_race = average_unemployment_by_race.filter(
+        items=['Asian', 'Black', 'White', 'Mixed', 'Other'])
     average_unemployment_by_race = average_unemployment_by_race.dropna(how='all')
     return average_unemployment_by_race
 
 
-def process_oecd_education_data(file_path: str):
-    df_oecd_education = pd.read_csv(file_path)
+def process_oecd_education_data(df_oecd_education: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adjusts the education level descriptions to a more generic classification and computes the average
+    unemployment rates by these categories for European countries listed in a global list 'european_countries'.
+    The DataFrame is then pivoted for easy visualization and analysis.
+
+    Parameters:
+    df_oecd_education (pd.DataFrame): A DataFrame containing columns 'Country', 'Education Level', and 'Value'
+                                      where 'Value' typically represents an unemployment rate.
+
+    Returns:
+    pd.DataFrame: A DataFrame suitable for plotting that includes the country, remapped education levels, and
+                  their corresponding average unemployment rates. The DataFrame is indexed by 'Country' and
+                  'General Education Level'.
+
+    DocTest:
+    >>> data = {'Country': ['France', 'Germany', 'Spain', 'Italy'],
+    ...         'Education Level': ['Doctoral or equivalent education', 'Upper secondary education',
+    ...                             'Less than primary education', 'Master’s or equivalent education'],
+    ...         'Value': [5, 15, 10, 8]}
+    >>> df_oecd_education = pd.DataFrame(data)
+    >>> processed_data = process_oecd_education_data(df_oecd_education)
+    >>> processed_data
+            Country                    Education Level  Unemployment Rate
+    0    France                    Doctoral degree                5.0
+    1   Germany                    Doctoral degree                NaN
+    2     Italy                    Doctoral degree                NaN
+    3     Spain                    Doctoral degree                NaN
+    4    France  High school graduates, no college                NaN
+    5   Germany  High school graduates, no college               15.0
+    6     Italy  High school graduates, no college                NaN
+    7     Spain  High school graduates, no college                NaN
+    8    France    Less than a high school diploma                NaN
+    9   Germany    Less than a high school diploma                NaN
+    10    Italy    Less than a high school diploma                NaN
+    11    Spain    Less than a high school diploma               10.0
+    12   France                    Master's degree                NaN
+    13  Germany                    Master's degree                NaN
+    14    Italy                    Master's degree                8.0
+    15    Spain                    Master's degree                NaN
+    """
     df_oecd_education.rename(columns={'ISCED 2011 A education level': 'Education Level'}, inplace=True)
     # # Group the data by 'Country' and 'Education Level', to calculate the average unemployment rate over years
     # df_oecd_education = df_oecd_education.groupby(['Country', 'Education Level'])['Value'].mean().reset_index()
@@ -129,18 +207,14 @@ def process_oecd_education_data(file_path: str):
     df_oecd_education_europe = df_oecd_education[df_oecd_education['Country'].isin(european_countries)]
     # Reset index after filtering
     df_oecd_education_europe = df_oecd_education_europe.reset_index(drop=True)
-
     # Reference the global variable dictionary to map ISCED defined education levels to the generic education levels
     global isced_education_mapping
-
     # Apply the mapping to create a new 'General Education Level' column
     df_oecd_education_europe['General Education Level'] = df_oecd_education_europe['Education Level'].map(
         isced_education_mapping)
-
     # Group by the 'Country' and the new 'Generic Education Level' and calculate the mean of 'Value'
     df_oecd_education_grouped = \
-    df_oecd_education_europe.groupby(['Country', 'General Education Level'], as_index=False)['Value'].mean()
-
+        df_oecd_education_europe.groupby(['Country', 'General Education Level'], as_index=False)['Value'].mean()
     # Pivot the dataframe to reshape it for plotting
     pivot_df = df_oecd_education_grouped.pivot(index='Country', columns='General Education Level', values='Value')
     # Reset index to make 'Country' a column again
@@ -158,18 +232,21 @@ def process_oecd_education_data(file_path: str):
     return melted_df
 
 
-def process_oecd_specific_countries(file_path: str, countries: list):
-    df_oecd = pd.read_csv(file_path)
-    df_oecd.rename(columns={'ISCED 2011 A education level': 'Education Level', 'Value' : 'Unemployment Rate'}, inplace=True)
-    df_oecd_countries = df_oecd[df_oecd['Country'].isin(countries)][['Country', 'YEAR', 'Education Level', 'Unemployment Rate']]
+def process_oecd_specific_countries(df_oecd: pd.DataFrame, countries: list):
+    df_oecd.rename(columns={'ISCED 2011 A education level': 'Education Level', 'Value': 'Unemployment Rate'},
+                   inplace=True)
+    df_oecd_countries = df_oecd[df_oecd['Country'].isin(countries)][
+        ['Country', 'YEAR', 'Education Level', 'Unemployment Rate']]
 
     # Reference the global variable dictionary to map ISCED defined education levels to the generic education levels
     global isced_education_mapping
 
     # Normalize the 'Education Level' values in the DataFrame and then map
-    df_oecd_countries['General Education Level'] = df_oecd_countries['Education Level'].str.strip().map(isced_education_mapping)
+    df_oecd_countries['General Education Level'] = df_oecd_countries['Education Level'].str.strip().map(
+        isced_education_mapping)
 
-    df_oecd_countries = df_oecd_countries.groupby(['Country','YEAR', 'General Education Level'])['Unemployment Rate'].mean().reset_index()
+    df_oecd_countries = df_oecd_countries.groupby(['Country', 'YEAR', 'General Education Level'])[
+        'Unemployment Rate'].mean().reset_index()
     global education_level_order
     # Convert the 'Education Level' column to a categorical type with the specified order for better viewing
     df_oecd_countries['Education Level'] = pd.Categorical(
@@ -177,12 +254,13 @@ def process_oecd_specific_countries(file_path: str, countries: list):
         categories=education_level_order,
         ordered=True
     )
-    df_oecd_countries = df_oecd_countries.pivot(index=['Country', 'YEAR'], columns='Education Level', values='Unemployment Rate')
+    df_oecd_countries = df_oecd_countries.pivot(index=['Country', 'YEAR'], columns='Education Level',
+                                                values='Unemployment Rate')
     return df_oecd_countries
 
 
-def process_StatsGovCN_data(educate_year_file_path: str, unemployment_file_path: str):
-    china_educated_year = pd.read_excel(educate_year_file_path)[
+def process_StatsGovCN_data(china_educated_year: pd.DataFrame, unemployment_file_path: str):
+    china_educated_year = china_educated_year[
         ['year', 'province', '人均受教育年限']].rename(columns={'人均受教育年限': 'Average Educated Years'})
     china_national_educated_year = china_educated_year[china_educated_year['province'] == '全国'][
         ['year', 'Average Educated Years']]
